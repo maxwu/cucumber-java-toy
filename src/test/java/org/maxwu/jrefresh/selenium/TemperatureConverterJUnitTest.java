df95919861d8b92cb11d2e889d973613c51a610c
maxwu <maxwunj@gmail.com>
2017-01-10 23:49:51 +1300

    Add an experiment in JUnit

--- src/test/java/org/maxwu/jrefresh/selenium/TemperatureConverterJUnitTest.java
+++ src/test/java/org/maxwu/jrefresh/selenium/TemperatureConverterJUnitTest.java
@@ -1,28 +1,37 @@
 package org.maxwu.jrefresh.selenium;
 
-import cucumber.api.java.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.maxwu.jrefresh.ColorPrint;
+import org.openqa.selenium.WebDriver;
 
 /**
  * Created by maxwu on 1/5/17.
+ * This class is designed to demo pure JUnit way of test.
+ * (Instead of Cucumber-JVM).
  */
 public class TemperatureConverterJUnitTest {
+    private WebDriver driver = null;
+
     @Before
     public void setUp(){
-        ColorPrint.println_blue("Starting JUnit 2nd phase test");
+        driver = DriverFactory.getDriver();
     }
 
     @Test
-    public void verify_true(){
-        ColorPrint.println_blue("Sample Case-1");
+    public void get_one_page(){
         // This is the sample case driven by JUnit.
         // The real tests were done with Cucumber JVM and Selenium Page Object.
+        driver.get("http://www.facebook.com");
+    }
+
+    @Test
+    public void get_one_page_after1(){
+        driver.get("http://www.google.com");
     }
 
     @After
     public void tearDown(){
-        ColorPrint.println_blue("The tear down step");
+        ColorPrint.println_blue("The tear down step for JUnit");
+        driver.quit();
     }
 }
